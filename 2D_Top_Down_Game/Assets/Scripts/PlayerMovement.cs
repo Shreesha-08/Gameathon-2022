6c4c802dd8bcd3071bda5f4b9f4747d1b92afbe8
Prajwal Kulkarni <kulkarniprajwal.01@gmail.com>
2022-11-25 16:07:34 +0530

    updated player movements

--- 2D_Top_Down_Game/Assets/Scripts/PlayerMovement.cs
+++ 2D_Top_Down_Game/Assets/Scripts/PlayerMovement.cs
@@ -6,23 +6,32 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
+    public Camera camera;
+
     Vector2 movement;
+    Vector2 mousePos;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        camera = Camera.main;
     }
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 
 }
