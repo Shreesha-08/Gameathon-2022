7958e6b97397c795868e61d913484de726b04a15
Akshay <akshayrgowda2001@gmail.com>
2022-11-25 23:40:54 +0530

    More dynamic shooter

--- 2D_Top_Down_Game/Assets/Scripts/Bullet.cs
+++ 2D_Top_Down_Game/Assets/Scripts/Bullet.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public GameObject hitEffect;
+    public int damage=1;
     void OnCollisionEnter2D(Collision2D collision){
         var enemy = collision.collider.GetComponent<Enemy>();
         if (enemy)
         {
-            enemy.TakeHit(1);
+            enemy.TakeHit(damage);
         }
         GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
--- 2D_Top_Down_Game/Assets/Scripts/Shooting.cs
+++ 2D_Top_Down_Game/Assets/Scripts/Shooting.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+	public int fireRate = 0;
     public Transform firePoint;
+	public int damage = 3;
+	public int critChance = 1;
+	public int clipSize = 6;
+	public static int shotsFired = 0;
+	float timeToFire = 0;
+	public float reloadTime = 1f;
+	
+	private bool reloading = false;
     public GameObject bluebulletPrefab;
 
 	public float bulletForce = 20f;
@@ -12,15 +21,54 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1")){
-            Shoot();
-        }
+
+		if (Input.GetKeyDown (KeyCode.R)) {
+			StartCoroutine ("Reload");
+			reloading = true;
+		}
+
+		if (reloading)
+			return;
+
+		if (shotsFired >= clipSize) {
+			StartCoroutine ("Reload");
+			reloading = true;
+			return;
+		}
+
+		if (fireRate == 0) {
+			if (Input.GetButtonDown ("Fire1")) {
+				Shoot();
+				shotsFired++;
+			}
+		}
+		else {
+			if (Input.GetButton ("Fire1")) {
+				if (Time.time > timeToFire) {
+					timeToFire = Time.time + 1/(float)fireRate;
+					Shoot();
+					shotsFired++;
+				}
+			}
+		}
     }
 
     void Shoot () 
     {
         GameObject bullet = Instantiate(bluebulletPrefab, firePoint.position, firePoint.rotation);
+        
+		float dmg = damage;
+	
+		if (Random.Range (0, 101) <= critChance)
+			dmg *= Random.Range(2f, 3f);
+        bullet.GetComponent<Bullet>().damage = (int)dmg;
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+	}
+    
+	IEnumerator Reload () {
+		yield return new WaitForSeconds (reloadTime);
+		shotsFired = 0;
+		reloading = false;
 	}
 }
